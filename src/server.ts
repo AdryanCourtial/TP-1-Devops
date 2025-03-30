eae5a88eb4b7a1b98f5386ba580dc7748eaa4d0e
AdryanCourtial <adryan.courtial@ynov.com>
2025-03-30 21:08:00 +0200

    :construction: ajout mini framework

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,32 @@
-import http from 'http';
-import { stringify } from 'querystring';
+import { getEnv } from './env';
+import { App } from './services';
 
-const hostname = '0.0.0.0';
-const port = 3000;
+// Ajout des variables d'environement
+const hostname = getEnv<string>("SERVER_HOST");
+const port = getEnv<number>("SERVER_PORT");
+
 const serverUrl = `http://${hostname}:${port}`
 
-const server = http.createServer((req, res) => {
-  
-  if (req.url === "/ping" && req.method === "GET") {
-    const header = req.headers
-    return res.end(stringify(header))
-  }
+// Construction de l'application
+const app = new App(port, hostname)
 
-  res.writeHead(404).end()
 
-});
+// Route que vous voulez et qui été demandé
+app.Get("/ping", (req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(JSON.stringify(req.headers));
+})
 
+// Je sais pas j'avais envie x)
+app.Get("/test", (req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(JSON.stringify({ message: "test" }));
+})
+
+// Génaration du Server lorsque toutes les fonctions au dessus ont définies les routes
+const server = app.GenerateServer()
+
+// On Ecoute sur le Server Généré a l'instant
 server.listen(port, hostname, () => {
-  console.log(`Server running at ${serverUrl}`);
+  console.log(`Lancement du server sur le port : ${serverUrl}`);
 });
